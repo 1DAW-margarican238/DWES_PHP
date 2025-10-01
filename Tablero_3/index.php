e40e94462c4f2296b9f4ca63005db7711eed55f9
unknown <usuario@MVWinProx.iesmartinezm.es>
2025-10-01 14:56:35 +0200

    act profesor + personaje

--- Tablero_3/index.php
+++ Tablero_3/index.php
@@ -1,63 +1,41 @@
 <?php
 
-/* Inicialización del entorno */
+/***** Inicialización del entorno ******/
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 /* Zona de declaración de funciones */
-//Funciones de debugueo
+//*******Funciones de debugueo****
 function dump($var){
     echo '<pre>'.print_r($var,1).'</pre>';
 }
 
-
-
-
-//Función lógica presentación
-function getTableroMArkup ($tablero, $ponerPersonaje){
-    $cont =0;
+//*******Función lógica presentación**********+
+function getTableroMarkup ($tablero, $posPersonaje){
     $output = '';
-    //dump($tablero);
     foreach ($tablero as $filaIndex => $datosFila) {
         foreach ($datosFila as $columnaIndex => $tileType) {
-            //dump($tileType);
-            $cont++;
-            if($cont == $ponerPersonaje){
-            $output .= '<div class = "tile ' . $tileType . '"> <img src="betis.png" width="50px"; heihgt"50px";/></div>';
+            if(isset($posPersonaje)&&($filaIndex == $posPersonaje['row'])&&($columnaIndex == $posPersonaje['col'])){
+                $output .= '<div class = "tile ' . $tileType . '"><img src="betis.png"></div>';    
             }else{
-             $output .= '<div class = "tile ' . $tileType . '"></div>';
+                $output .= '<div class = "tile ' . $tileType . '"></div>';
             }
         }
     }
     return $output;
 }
-
-
-   $ponerPersonaje = null;
-
-if((isset($_GET ['fila']) && isset($_GET ['col']))&&(($_GET ['fila']) <13  && ($_GET ['col'])<13)&&(($_GET ['fila']) >0  && ($_GET ['col'])>0)){
-    $fila = $_GET ['fila'];
-    $col = $_GET ['col'];
-    $ponerPersonaje = (($fila*12-12) + ($col));
-}
-
-function posicionInvalida($ponerPersonaje){
-    if(!isset($ponerPersonaje)){
-        echo '<p>La posición es nula, menor que 1 o mayor que 12.</p>';
-    }
-}
-
-//Lógica de negocio
+//******+Función Lógica de negocio************
 //El tablero es un array bidimensional en el que cada fila contiene 12 palabras cuyos valores pueden ser:
 // agua
 //fuego
 //tierra
 // hierba
-function leerArchivoCSV($archivoCSV) {
+function leerArchivoCSV($rutaArchivoCSV) {
     $tablero = [];
 
-    if (($puntero = fopen($archivoCSV, "r")) !== FALSE) {
+    if (($puntero = fopen($rutaArchivoCSV, "r")) !== FALSE) {
         while (($datosFila = fgetcsv($puntero)) !== FALSE) {
             $tablero[] = $datosFila;
         }
@@ -66,11 +44,30 @@ function leerArchivoCSV($archivoCSV) {
 
     return $tablero;
 }
+function leerInput(){
+    
+    $col = filter_input(INPUT_GET, 'col', FILTER_VALIDATE_INT);
+    $row = filter_input(INPUT_GET, 'row', FILTER_VALIDATE_INT);
 
-$tablero = leerArchivoCSV('contenido_tablero/contenido.csv');
+    return (isset($col) && isset($row) && is_numeric($col) && is_numeric($row))? array(
+            'row' => $row,
+            'col' => $col
+        ) : null;    
+}
+//*****Lógica de negocio***********
+//Extracción de las variables de la petición
 
-//Lógica de presentación
-$tableroMarkup = getTableroMArkup($tablero, $ponerPersonaje);
+
+$posPersonaje = leerInput();
+
+dump('$posPersonaje');
+dump($posPersonaje);
+$tablero = leerArchivoCSV('./contenido_tablero/contenido.csv');
+
+
+
+//*****+++Lógica de presentación*******
+$tableroMarkup = getTableroMarkup($tablero, $posPersonaje);
 
 
 ?>
@@ -81,48 +78,46 @@ $tableroMarkup = getTableroMArkup($tablero, $ponerPersonaje);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Minified version -->
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
-
     <title>Document</title>
     <style>
         .contenedorTablero {
-           max-width: 600px;
-            width: 100%;
-            height: 600px;
-            border-radius: 5px;
+            width:600px;
+            height:600px;
             border: solid 2px grey;
             box-shadow: grey;
             display:grid;
-            grid-template-rows:repeat(12, 1fr);
-            grid-template-columns:repeat(12, 1fr);
+            grid-template-columns: repeat(12, 1fr);
+            grid-template-rows: repeat(12, 1fr);
         }
         .tile {
-            /* width: 50px; */
-            /* height: 50px; */
             float: left;
             margin: 0;
             padding: 0;
             border-width: 0;
-            background-image: url('464.jpg');
+            background-image: url("464.jpg");
             background-size: 209px;
+            background-repeat: none;
+            overflow: hidden;
+        }
+        .tile img{
+            max-width:100%;
         }
         .fuego {
-        background-position: -105px -52px;
-
+            background-color: red;
+            background-position: -105px -52px;
         }
         .tierra {
-        background-position: -157px 0px;
+            background-color: brown;
+            background-position: -157px 0px;
         }
         .agua {
-        background-position: -53px 0px;
+            background-color: blue;
+            background-position: -53px 0px;
         }
         .hierba {
-        background-position: 0px 0px;
+            background-color: green;
+            background-position: 0px 0px;
         }
-
-        .mensaje{
-        font-weight: bold;
-        }
-
     </style>
 </head>
 <body>
@@ -130,12 +125,5 @@ $tableroMarkup = getTableroMArkup($tablero, $ponerPersonaje);
     <div class="contenedorTablero">
         <?php echo $tableroMarkup; ?>
     </div>
-    <div class="mensaje">
-    <?php
-        echo posicionInvalida($ponerPersonaje);
-    ?>
-    </div>
-
-   
 </body>
 </html>
